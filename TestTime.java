b243373120825fd9ccab8df6082014917ede0a24
sara woodford <sarawoodford6@gmail.com>
2024-09-22 11:33:56 -0230

    Creating TestTime.java
    creating 2 time objects and calling nextSecond()
    and previousSecond(), then displaying altered
    times.

--- TestTime.java
+++ TestTime.java
@@ -1,3 +1,16 @@
 public class TestTime {
-    
+    public static void main(String[] args){
+        // create time object
+        Time myTime1 = new Time(21, 10, 15);
+        Time myTime2 = new Time(10, 20, 25);
+    // print inital times
+    System.out.println("time 1: " + myTime1.toString());
+    System.out.println("time 2: " + myTime2.toString());
+
+    myTime1.nextSecond();
+    myTime2.previousSecond();
+
+    System.out.println("time 1 after nextSecond() called: " + myTime1.toString());
+    System.out.println("time 2 after previousSecond() called: " + myTime2.toString());
+    }
 }

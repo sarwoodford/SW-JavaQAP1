e977f67bb480b2f9f6032de16e667bc198155198
sara woodford <sarawoodford6@gmail.com>
2024-09-22 10:55:44 -0230

    spelling mistake fixed
    misspelled "main"

--- TestDate.java
+++ TestDate.java
@@ -1,5 +1,5 @@
 public class TestDate {
-    public static void mian(String[] args) {
+    public static void main(String[] args) {
         //create date object
         Date myDate1 = new Date(7, 11, 2004);
         Date myDate2 = new Date(29, 04, 2023);
